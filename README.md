9a7eae40b65c49485d96ceaaf5c956c86123ad4b
fkei <kei.topaz@gmail.com>
2021-07-19 10:32:08 +0900

    Update: ✨ Change to a simple sample code version

--- README.md
+++ README.md
@@ -1,38 +1,32 @@
 # removesilence
 
-removesilence: remove silent segments from a video file using ffmpeg.
+// forked from jordancurve/removesilence
+
+Sample code that cuts the front and back silence in the video using `ffmpeg`.
+
+> ja. 動画中の前後の無音部分をカットするサンプルコード
+
+Dependence
+
+- Commend : `ffmpeg`
 
 Usage of removesilence:
-```
+
+```bash
 Required flags:
   -infile string
       Path to input video file.
   -outfile string
       Path to output video file.
-  -maxpause float
-      max allowable period of silence (seconds). Any silent segment longer than
-      this will be trimmed down to exactly this length by removing the middle
-      portion and leaving maxpause/2 seconds of padding on each side.
   -silencedb float
       volume level (dB) below which audio is considered to be silence.
       Usually negative (e.g. -30).
-
-Optional flags:
-
-  -minkeep float
-    	minimum length of segment to include (including any padding).
-  -keep-temp-files
-      keep temp files
-  -intropadding float
-    	number of seconds of video to keep before you start talking for the first time.
-  -outropadding float
-    	number of seconds of video to keep after you stop talking for the last time.
 ```
 
 Example:
 
-```
+```bash
 go build removesilence.go
 
-./removesilence -infile in.mp4 -outfile out.mp4  -maxpause 2 -silencedb -30 -minkeep 2.4
-``` 
+./removesilence -infile in.mp4 -outfile out.mp4 -silencedb -30
+```
--- removesilence.go
+++ removesilence.go
@@ -1,4 +1,3 @@
-// removesilence removes periods of silence from a video file using ffmpeg.
 package main
 
 import (
@@ -6,136 +5,142 @@ import (
 	"errors"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"os/exec"
-	"path/filepath"
 	"strconv"
 	"strings"
 )
 
-type segment struct {
-	start, end float64
+type options struct {
+	infile    string
+	outfile   string
+	silencedb float64
 }
 
-var keepTempFiles bool
+type period struct {
+	start float64
+	end   float64
+}
 
 func main() {
-	inFile := flag.String("infile", "", "Path to input video file.")
-	outFile := flag.String("outfile", "", "Path to output video file.")
-	silenceDb := flag.Float64("silencedb", 0.0,
-		"volume level (dB) below which audio is considered to be silence. "+
-			"Usually negative (e.g. -30).")
-	introPadding := flag.Float64("intropadding", 0.0,
-		"number of seconds of video to keep before you start talking for the first time. ")
-	outroPadding := flag.Float64("outropadding", 0.0,
-		"number of seconds of video to keep after you stop talking for the last time. ")
-	minKeep := flag.Float64("minkeep", 0.0, "minimum length of segment to include (including any padding).")
-	maxPause := flag.Float64("maxpause", 0.0, "max allowable period of silence "+
-		"aside from intro/outro (in seconds). Any silent segment longer than this "+
-		"will be trimmed down to exactly this length by removing the middle "+
-		"portion and leaving maxpause/2 seconds of padding on each side. After this is done, "+
-		"any remaining clips shorter than maxpause+1 seconds will be further shortened.")
-	flag.BoolVar(&keepTempFiles, "keep-temp-files", false, "keep temp files")
-
-	flag.Parse()
-	if err := doit(*inFile, *outFile, *maxPause, *minKeep, *silenceDb, *introPadding, *outroPadding); err != nil {
+	opts, err := getOptions()
+	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %s\n", err)
-		os.Exit(1)
+		os.Exit(2)
 	}
+	fmt.Fprintf(os.Stdout, "options: %v\n", opts)
+
+	// survey
+	p, err := survey(opts)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "survey error.\n")
+	}
+
+	// cut
+	if err := cut(opts, p); err != nil {
+		fmt.Fprintf(os.Stderr, "cut error.\n")
+	}
+
+	// end
+	fmt.Fprintf(os.Stdout, "output %s\n", opts.outfile)
 }
 
-func doit(inFile, outFile string, maxPause, minKeep, silenceDb float64, introPadding, outroPadding float64) error {
-	if inFile == "" {
-		return errors.New("-infile is required")
-	}
-	if outFile == "" {
-		return errors.New("-outfile is required")
-	}
-	if maxPause == 0.0 {
-		return errors.New("-maxpause is required")
-	}
-	if silenceDb == 0.0 {
-		return errors.New("-silencedb is required")
-	}
-
-	tmpDir, err := ioutil.TempDir("/tmp", "rs")
-	if err != nil {
-		return err
-	}
-	if !keepTempFiles {
-		defer os.RemoveAll(tmpDir)
-	}
-	cmd := exec.Command("nice", "ffmpeg",
-		"-i", inFile,
-		"-filter_complex", fmt.Sprintf("[0:a]silencedetect=n=%gdB:d=%g[outa]", silenceDb, 0.1),
+func survey(opts *options) (*period, error) {
+	cmd := exec.Command("time", "ffmpeg",
+		// "-v", "trace",
+		"-i", opts.infile,
+		"-filter_complex", fmt.Sprintf("[0:a]silencedetect=noise=%gdB:duration=%g[outa]", opts.silencedb, 0.1),
 		"-map", "[outa]",
 		"-f", "s16le",
 		"-y", "/dev/null",
 	)
-	lines, err := commandStderrLines(cmd)
+
+	fmt.Fprintf(os.Stdout, "command(survey): %s\n", cmd.Args)
+
+	lines, err := getCmdResults(cmd)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, strings.Join(lines, "\n")+"\n")
-		return err
+		return nil, err
 	}
-	dur, err := ffmpegParseDuration(lines)
+	duration, _ := getDuration(lines)
+	fmt.Fprintf(os.Stdout, "duration %v\n", duration)
+
+	p, err := getPeriod(lines)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	silence, err := ffmpegParseSilentPeriods(lines)
-	if err != nil {
-		return err
-	}
-	keep := segmentsToKeep(silence, maxPause, introPadding, outroPadding, dur)
-	if minKeep > 0.0 {
-		keep = stripSegmentsShorterThan(keep, minKeep)
-	}
-	shortenPaddingForShortClips(keep, maxPause)
-	fmt.Printf("keeping segments: %v\n", keep)
-	chunks, err := cut(inFile, keep, tmpDir)
-	if err != nil {
-		return err
-	}
-	return join(chunks, outFile, tmpDir)
+	fmt.Fprintf(os.Stdout, "period %v\n", p)
+
+	return p, nil
 }
 
-func shortenPaddingForShortClips(keep []segment, maxPause float64) {
-	p := maxPause / 2 // padding
-	for i, c := range keep {
-		if i == 0 || i == len(keep)-1 {
+func cut(opts *options, p *period) error {
+
+	cmd := exec.Command("time", "ffmpeg",
+		// "-v", "trace",
+		"-ss", strconv.FormatFloat(p.start, 'f', 2, 64),
+		"-i", opts.infile,
+		"-t", strconv.FormatFloat(p.end-p.start, 'f', 2, 64),
+		"-vcodec", "copy", // speed up
+		"-acodec", "copy", // speed up
+		"-y",
+		opts.outfile,
+	)
+	fmt.Fprintf(os.Stdout, "command(cut): %s\n", cmd.Args)
+
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getPeriod(lines []string) (*period, error) {
+
+	p := &period{
+		start: 0.0,
+		end:   0.0,
+	}
+	// fmt.Fprintf(os.Stdout, "lines %v\n", lines)
+
+	for i := len(lines) - 1; i >= 0; i-- {
+		words := strings.Split(lines[i], " ")
+		if len(words) < 5 {
 			continue
 		}
-		if c.duration() < 2*p+1 {
-			newPad := c.duration() - 2*p
-			padDiff := p - newPad
-			if newPad < p {
-				keep[i].start += padDiff
-				keep[i].end -= padDiff
-				fmt.Printf("shortened padding: %s --> %s\n", c, keep[i])
+		if words[3] == "silence_start:" {
+			// fmt.Fprintf(os.Stdout, "silence_start %v\n", words[4])
+			v, err := strconv.ParseFloat(words[4], 64)
+			if err != nil {
+				return nil, err
 			}
+			// fmt.Fprintf(os.Stdout, "end %v\n", v)
+			p.end = v
+			break
 		}
 	}
-}
 
-func stripSegmentsShorterThan(segs []segment, thresh float64) []segment {
-	keep := []segment{}
-	for _, s := range segs {
-		if s.duration() >= thresh {
-			keep = append(keep, s)
+	for _, line := range lines {
+		words := strings.Split(line, " ")
+		if len(words) < 5 {
+			continue
+		}
+		if words[3] == "silence_start:" {
+			// fmt.Fprintf(os.Stdout, "silence_start %v\n", words[4])
+			v, err := strconv.ParseFloat(words[4], 64)
+			if err != nil {
+				return nil, err
+			}
+			// fmt.Fprintf(os.Stdout, "end %v\n", v)
+			p.start = v
+			break
 		}
 	}
-	return keep
+
+	return p, nil
 }
 
-func (s segment) duration() float64 {
-	return s.end - s.start
-}
-
-func segmentsToKeep(silence []segment, maxPause, introPadding, outroPadding, dur float64) []segment {
-	return invertSegments(segmentsToRemove(silence, maxPause, introPadding, outroPadding, dur))
-}
-
-func ffmpegParseDuration(lines []string) (float64, error) {
+func getDuration(lines []string) (float64, error) {
 	for _, line := range lines {
 		fields := strings.Fields(line)
 		if fields[0] != "Duration:" {
@@ -148,7 +153,7 @@ func ffmpegParseDuration(lines []string) (float64, error) {
 		}
 		return s, nil
 	}
-	return 0.0, errors.New("Couldn't find duration in ffmpeg output")
+	return 0.0, errors.New("couldn't find duration in ffmpeg output")
 }
 
 func parseHMS(hms string) (float64, error) {
@@ -168,129 +173,7 @@ func parseHMS(hms string) (float64, error) {
 	return 60*60*h + 60*m + s, nil
 }
 
-func join(inFiles []string, outFile, tmpDir string) error {
-	logFile, err := os.Create(filepath.Join(tmpDir, "concat.log"))
-	if err != nil {
-		return err
-	}
-	lines := []string{}
-	for _, f := range inFiles {
-		lines = append(lines, fmt.Sprintf("file '%s'\n", f))
-	}
-	fileList := filepath.Join(tmpDir, "list.txt")
-	if err := ioutil.WriteFile(fileList, []byte(strings.Join(lines, "")), 0600); err != nil {
-		return err
-	}
-	cmd := exec.Command(
-		"nice",
-		"ffmpeg",
-		"-f", "concat",
-		"-safe", "0",
-		"-i", fileList,
-		"-c", "copy",
-		"-map", "0",
-		"-y",
-		outFile,
-	)
-	cmd.Stderr = logFile
-	cmd.Stdout = logFile
-	fmt.Printf("%s\n", strings.Join(cmd.Args, " "))
-	return cmd.Run()
-}
-
-func cut(inFile string, keep []segment, tmpDir string) ([]string, error) {
-	chunks := []string{}
-	logFilePath := filepath.Join(tmpDir, "extract.log")
-	logFile, err := os.Create(logFilePath)
-	if err != nil {
-		return nil, err
-	}
-	ext := filepath.Ext(inFile)
-	for i, k := range keep {
-		args := []string{"ffmpeg", "-v", "error"}
-		if k.start != 0 {
-			args = append(args, "-ss", fmt.Sprintf("%f", k.start))
-		}
-		args = append(args, "-i", inFile)
-		if k.end != 0 {
-			args = append(args, "-t", fmt.Sprintf("%f", k.end-k.start))
-		}
-		args = append(args, "-acodec", "copy")
-		chunk := filepath.Join(tmpDir, fmt.Sprintf("%d%s", i+1, ext))
-		chunks = append(chunks, chunk)
-		args = append(args, chunk)
-		cmd := exec.Command("nice", args...)
-		cmd.Stdout = logFile
-		cmd.Stderr = logFile
-		fmt.Printf("%s\n", strings.Join(cmd.Args, " "))
-		if err := cmd.Run(); err != nil {
-			showFile(logFilePath)
-			return nil, err
-		}
-	}
-	return chunks, nil
-}
-
-func segmentsToRemove(silence []segment, maxPause, introPadding, outroPadding, dur float64) []segment {
-	rem := []segment{}
-	if len(silence) == 0 {
-		return []segment{}
-	}
-	if introPadding > 0 && silence[0].start <= 0 && silence[0].end > introPadding {
-		rem = append(rem, segment{0, silence[0].end - introPadding})
-	}
-	for _, s := range silence[1:] {
-		if s.duration() > maxPause && s.end < dur {
-			rem = append(rem, segment{s.start + maxPause/2.0, s.end - maxPause/2.0})
-		}
-	}
-	e := silence[len(silence)-1]
-	if outroPadding > 0 && e.end >= dur && e.duration() > outroPadding {
-		rem = append(rem, segment{e.start + outroPadding, 0.0})
-	}
-	return rem
-}
-
-func invertSegments(segs []segment) []segment {
-	end := 0.0
-	inv := []segment{}
-	fmt.Printf("inverting %v\n", segs)
-	for _, s := range segs {
-		if s.start > 0.0 {
-			inv = append(inv, segment{end, s.start})
-		}
-		end = s.end
-	}
-	inv = append(inv, segment{end, 0.0})
-	return inv
-}
-
-func ffmpegParseSilentPeriods(lines []string) ([]segment, error) {
-	silence := []segment{}
-	for _, line := range lines {
-		words := strings.Split(line, " ")
-		if len(words) < 5 {
-			continue
-		}
-		tag, val := words[3], words[4]
-		loc, err := strconv.ParseFloat(val, 64)
-		if err != nil {
-			continue
-		}
-		switch tag {
-		case "silence_start:":
-			silence = append(silence, segment{loc, 0.0})
-		case "silence_end:":
-			if len(silence) == 0 {
-				return nil, errors.New("ffmpeg output: silence_end before silence_start")
-			}
-			silence[len(silence)-1].end = loc
-		}
-	}
-	return silence, nil
-}
-
-func commandStderrLines(cmd *exec.Cmd) ([]string, error) {
+func getCmdResults(cmd *exec.Cmd) ([]string, error) {
 	stderr, err := cmd.StderrPipe()
 	if err != nil {
 		return nil, err
@@ -298,8 +181,11 @@ func commandStderrLines(cmd *exec.Cmd) ([]string, error) {
 	if err := cmd.Start(); err != nil {
 		return nil, err
 	}
+
 	scanner := bufio.NewScanner(stderr)
+
 	lines := []string{}
+
 	for scanner.Scan() {
 		lines = append(lines, scanner.Text())
 	}
@@ -312,17 +198,27 @@ func commandStderrLines(cmd *exec.Cmd) ([]string, error) {
 	return lines, nil
 }
 
-func (s segment) String() string {
-	o := fmt.Sprintf("%.2f-", s.start)
-	if s.end != 0.0 {
-		o += fmt.Sprintf("%.2f", s.end)
-	}
-	return o
-}
+func getOptions() (*options, error) {
+	infile := flag.String("infile", "", "Path to input video file.")
+	outfile := flag.String("outfile", "", "Path to output video file.")
+	silencedb := flag.Float64("silencedb", -30.0, "V volume level (dB) below which audio is considered to be silence.	Usually negative (e.g. -30).")
 
-func showFile(path string) {
-	contents, err := ioutil.ReadFile(path)
-	if err == nil {
-		fmt.Print(string(contents))
+	flag.Parse()
+
+	// check
+	if *infile == "" {
+		return nil, errors.New("command-line options `-infile` is required")
 	}
+	if *outfile == "" {
+		return nil, errors.New("command-line options `-outfile` is required")
+	}
+	if *silencedb == 0.0 {
+		return nil, errors.New("command-line options `-silencedb` is required")
+	}
+
+	return &options{
+		infile:    *infile,
+		outfile:   *outfile,
+		silencedb: *silencedb,
+	}, nil
 }
